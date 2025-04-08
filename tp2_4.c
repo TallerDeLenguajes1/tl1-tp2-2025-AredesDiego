698e268e2969558446e1fabbc2e90fd338825bb3
Diego Aredes <aredesdiegofabian@gmail.com>
2025-04-08 09:23:08 -0300

    valores numeros aleatorios

--- tp2_4.c
+++ tp2_4.c
@@ -2,9 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+struct
+{
+    int velocidad;
+    int anio;
+    int cantidad_nucleos;
+    char *tipo_cpu;
+}typedef compu;
+
 int main()
 {
     srand(time(NULL));
+    compu Computadora;
+
+    //valores numeros aleatorios
+    Computadora.velocidad = rand() % 3 + 1;
+    Computadora.anio = rand() % 10 + 2015;
+    Computadora.cantidad_nucleos = rand() % 8 + 1;
+    
 
     return 0;
 }
